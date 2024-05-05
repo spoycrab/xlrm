9803b133c253414256046751cd354be1d7180b9e
Guilherme Bernardes <guilherme.bernardes@uvvnet.com.br>
2024-05-04 23:13:10 -0300

    Connect to the database

--- go/main.go
+++ go/main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
 )
+
+var db *sql.DB
+
+func main() {
+	config := mysql.Config{
+		User: os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net: "tcp",
+		Addr: "127.0.0.1:3306",
+		DBName: "xlrm",
+		/* ParseTime: true, */
+	}
+
+	var err error
+
+	db, err = sql.Open("mysql", config.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello from the other side!");
-}
-
-func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
 }
