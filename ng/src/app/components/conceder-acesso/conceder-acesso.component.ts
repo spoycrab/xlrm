5b34293186c91582fd5251ff837d0db4301bfcde
Guilherme Bernardes <guilherme.bernardes@uvvnet.com.br>
2024-05-20 22:12:39 -0300

    Fix permissions
    Let's keep our code clean.

--- ng/src/app/components/conceder-acesso/conceder-acesso.component.ts
+++ ng/src/app/components/conceder-acesso/conceder-acesso.component.ts
@@ -1,124 +1,114 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
 import { User } from '../../user';
 import { UserService } from '../../user.service';
-import { CommonModule } from '@angular/common';
-import Swal from 'sweetalert2';
-import { MatTableModule } from '@angular/material/table';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTableDataSource } from '@angular/material/table';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
-import { Router } from '@angular/router';
-
 
 @Component({
-  selector: 'app-conceder-acesso',
-  standalone: true,
-  imports: [CommonModule, MatTableModule, MatButtonModule, MatIconModule],
-  templateUrl: './conceder-acesso.component.html',
-  styleUrl: './conceder-acesso.component.css'
+    selector: 'app-conceder-acesso',
+    standalone: true,
+    imports: [CommonModule, MatButtonModule, MatIconModule, MatTableModule],
+    templateUrl: './conceder-acesso.component.html',
+    styleUrl: './conceder-acesso.component.css'
 })
 export class ConcederAcessoComponent {
+    users: User[] = [];
+    displayedColumns: string[] = ['name', 'status', 'action'];
+    dataSource = new MatTableDataSource<User>();
 
-  usuarios: User[] = [];
-  displayedColumns: string[] = ['name', 'status', 'action'];
-  dataSource = new MatTableDataSource<User>();
+    constructor(private userService: UserService, private router: Router) { }
 
-  constructor(private userService: UserService, private router: Router) { }
-
-  ngOnInit(): void {
-    this.userService.getAllUsersAllowedWihoutPermissions().subscribe(
-      (response) => {
-        if (Array.isArray(response)) {
-          this.usuarios = response;
-        } else if (response != null) {
-          this.usuarios = [response];
-        }
-      //  console.log(this.usuarios); // Certifique-se de que a variável usuarios seja uma matriz
-      },
-      (error) => {
-        console.error("Erro ao obter usuários não registrados:", error);
-        // Lidar com o erro, por exemplo, exibir uma mensagem de erro para o usuário
-      }
-    );
-  }
-  concederAcesso(usuario: User): void {
-    const id = usuario.id;
-    Swal.fire({
-      title: 'Selecione as permissões',
-      html: `
-        <input type="checkbox" id="produto" value="5">
-        <label for="produto">Produto</label><br>
-        <input type="checkbox" id="vendas" value="6">
-        <label for="vendas">Vendas</label><br>
-        <input type="checkbox" id="clientes" value="7">
-        <label for="clientes">Clientes</label><br>
-        <input type="checkbox" id="admin" value="3">
-        <label for="admin">Administrador</label><br>
-      `,
-      showCancelButton: true,
-      confirmButtonText: 'Confirmar',
-      cancelButtonText: 'Cancelar',
-      preConfirm: () => {
-        let permissions = '';
-
-        // Verificar quais opções foram selecionadas
-        if ((<HTMLInputElement>document.getElementById('produto')).checked) {
-          permissions += '5';
-        }
-        if ((<HTMLInputElement>document.getElementById('vendas')).checked) {
-          permissions += '6';
-        }
-        if ((<HTMLInputElement>document.getElementById('clientes')).checked) {
-          permissions += '7';
-        }
-        if ((<HTMLInputElement>document.getElementById('admin')).checked) {
-          permissions += '3';
-        }
-
-        return permissions;
-      }
-    }).then((result) => {
-      if (result.isConfirmed) {
-        let permissions = result.value;
-        console.log('Permissões selecionadas:', permissions);
-
-        permissions = parseInt(permissions, 10)
-        let data = {id, permissions}
-        console.log(data)
-
-
-        // Chamar setUserPermission com as novas permissões
-        this.userService.setUserPermission(data).subscribe(
-          () => {
-            console.log('Permissões atualizadas com sucesso!');
-            Swal.fire({
-              title: "Sucesso!",
-              text: "As permissões foram alteradas com sucesso",
-              icon: "success"
-            }).then(() => {
-              location.reload(); // Recarregar a página após exibir a mensagem de sucesso
-            });
-            // Aqui você pode realizar outras ações após a atualização das permissões
-          },
-          (error) => {
-            console.error('Erro ao atualizar permissões:', error);
-            Swal.fire({
-              title: "Erro!",
-              text: "Ocorreu um erro, por favor tente novamente!",
-              icon: "error"
-            }).then(() => {
-              location.reload(); // Recarregar a página após exibir a mensagem de sucesso
-            });
-            // Lidar com o erro, por exemplo, exibir uma mensagem de erro para o usuário
-          }
+    ngOnInit(): void {
+        this.userService.getAllUsersAllowedWihoutPermissions().subscribe(
+            (response) => {
+                if (Array.isArray(response)) {
+                    this.users = response;
+                } else if (response != null) {
+                    this.users = [response];
+                }
+            },
+            (error) => {
+                console.error("Erro ao obter usuários não registrados:", error);
+            }
         );
-      }
-    });
-  }
+    }
 
-  goToTelaInicio(): void {
-		this.router.navigate(['/telaInicio']);
-	  }
+    grantAccess(user: User): void {
+        const id = user.id;
+
+        Swal.fire({
+            title: 'Selecione as permissões',
+            html: `
+<input type="checkbox" id="cust">
+<label for="cust">Clientes</label><br>
+<input type="checkbox" id="product">
+<label for="product">Produto</label><br>
+<input type="checkbox" id="sale">
+<label for="sale">Vendas</label><br>
+<input type="checkbox" id="all">
+<label for="all">Administradbor</label><br>
+`,
+            showCancelButton: true,
+            confirmButtonText: 'Confirmar',
+            cancelButtonText: 'Cancelar',
+            preConfirm: () => {
+                let permissions = 0;
+
+                if ((<HTMLInputElement>document.getElementById("cust")).checked) {
+                    permissions |= 4;
+                }
+                if ((<HTMLInputElement>document.getElementById("product")).checked) {
+                    permissions |= 8;
+                }
+                if ((<HTMLInputElement>document.getElementById("sale")).checked) {
+                    permissions |= 16;
+                }
+                if ((<HTMLInputElement>document.getElementById("all")).checked) {
+                    permissions |= 32;
+                }
+
+                return permissions;
+            }
+        }).then((result) => {
+            if (result.isConfirmed) {
+                let permissions = parseInt(result.value);
+
+                if (isNaN(permissions)) {
+                    permissions = 0;
+                }
+
+                let data = {id, permissions}
+
+                this.userService.setUserPermission(data).subscribe(
+                    () => {
+                        Swal.fire({
+                            title: "Sucesso!",
+                            text: "As permissões foram alteradas com sucesso",
+                            icon: "success"
+                        }).then(() => {
+                            location.reload(); // Recarregar a página após exibir a mensagem de sucesso
+                        });
+                    },
+                    (error) => {
+                        Swal.fire({
+                            title: "Erro!",
+                            text: "Ocorreu um erro, por favor tente novamente!",
+                            icon: "error"
+                        }).then(() => {
+                            location.reload(); // Recarregar a página após exibir a mensagem de sucesso
+                        });
+                    }
+                );
+            }
+        });
+    }
+
+    goToTelaInicio(): void {
+	this.router.navigate(['/telaInicio']);
+    }
 }
